d2d3ce6d534b4ad6fc70b792514e2375b36423d7
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-08-01 11:14:22 +0800

    Completed tutorial

--- DragDropTrial/MainWindow.xaml.cs
+++ DragDropTrial/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace DragDropTrial
         {
             InitializeComponent();
         }
+
+
+        #region event handlers
+
+        private void panel_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("Object"))
+            {
+                // These Effects values are used in the drag source's
+                // GiveFeedback event handler to determine which cursor to display.
+                if (e.KeyStates == DragDropKeyStates.ControlKey)
+                {
+                    e.Effects = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effects = DragDropEffects.Move;
+                }
+            }
+        }
+
+        private void panel_Drop(object sender, DragEventArgs e)
+        {
+            // If an element in the panel has already handled in the drop,
+            // the panel should not also handle it.
+            if (e.Handled == false)
+            {
+                Panel _panel = (Panel)sender;
+                UIElement _element = (UIElement)e.Data.GetData("Object");
+
+                if (_panel != null && _element != null)
+                {
+                    // Get the panel that the element currently belongs to,
+                    // then remove if from that panel and add it the Children of 
+                    // the panel that its been dropped on.
+                    Panel _parent = (Panel)VisualTreeHelper.GetParent(_element);
+
+                    if (_parent != null)
+                    {
+                        if (e.KeyStates == DragDropKeyStates.ControlKey &&
+                            e.AllowedEffects.HasFlag(DragDropEffects.Copy))
+                        {
+                            Circle _circle = new Circle((Circle)_element);
+                            _panel.Children.Add(_circle);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Copy;
+                        }
+                        else if (e.AllowedEffects.HasFlag(DragDropEffects.Move))
+                        {
+                            _parent.Children.Remove(_element);
+                            _panel.Children.Add(_element);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Move;
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
